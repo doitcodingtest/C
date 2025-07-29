b6e2099244df91588ca2960409efef29a8d022b3
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2025-07-29 19:41:25 +0900

    Rename 2162_선분그룹.cpp to P2162_선분그룹.cpp

--- 기하/P2162_선분그룹.cpp
+++ 기하/P2162_선분그룹.cpp
@@ -22,7 +22,7 @@ int main()
     for (int i = 1; i <= N; i++) {
         cin >> L[i][0] >> L[i][1] >> L[i][2] >> L[i][3];
    
-        for (int j = 1; j < i; j++) { //ÀÌÀü¿¡ ÀúÀåµÈ ¼±ºÐµé°ú ±³Â÷ ¿©ºÎ È®ÀÎ
+        for (int j = 1; j < i; j++) { //ì´ì „ì— ì €ìž¥ëœ ì„ ë¶„ë“¤ê³¼ êµì°¨ ì—¬ë¶€ í™•ì¸
             if (isCross(L[i][0], L[i][1], L[i][2], L[i][3], L[j][0], L[j][1], L[j][2], L[j][3]) == true) {
                 Union(i, j);
             }
@@ -31,9 +31,9 @@ int main()
 
     int ans = 0, res = 0;
     for (int i = 1; i <= N; i++) {
-        if (parent[i] < 0) {  //À½¼öÀÌ¸é ¼±ºÐ±×·ìÀ» ´ëÇ¥ÇÏ´Â ºÎ¸ð(´ëÇ¥) ³ëµåÀÌ¹Ç·Î Ä«¿îÆ®
+        if (parent[i] < 0) {  //ìŒìˆ˜ì´ë©´ ì„ ë¶„ê·¸ë£¹ì„ ëŒ€í‘œí•˜ëŠ” ë¶€ëª¨(ëŒ€í‘œ) ë…¸ë“œì´ë¯€ë¡œ ì¹´ìš´íŠ¸
             ans++;
-            res = min(res, parent[i]); //À½¼öÀÇ Àý´ë °ªÀÌ ¼±ºÐ±×·ìÀÇ ¼±ºÐ °³¼ö
+            res = min(res, parent[i]); //ìŒìˆ˜ì˜ ì ˆëŒ€ ê°’ì´ ì„ ë¶„ê·¸ë£¹ì˜ ì„ ë¶„ ê°œìˆ˜
         }
     }
 
@@ -77,10 +77,10 @@ bool isCross(long x1, long y1, long x2, long y2, long x3, long y3, long x4, long
     int abd = CCW(x1, y1, x2, y2, x4, y4);
     int cda = CCW(x3, y3, x4, y4, x1, y1);
     int cdb = CCW(x3, y3, x4, y4, x2, y2);
-    if (abc * abd == 0 && cda * cdb == 0) { // ¼±ºÐÀÌ ÀÏÁ÷ ¼±ÀÎ °æ¿ì
+    if (abc * abd == 0 && cda * cdb == 0) { // ì„ ë¶„ì´ ì¼ì§ ì„ ì¸ ê²½ìš°
         return isOverlab(x1, y1, x2, y2, x3, y3, x4, y4);
     }
-    else if (abc * abd <= 0 && cda * cdb <= 0) { // ¼±ºÐÀÌ ±³Â÷ÇÏ´Â °æ¿ì
+    else if (abc * abd <= 0 && cda * cdb <= 0) { // ì„ ë¶„ì´ êµì°¨í•˜ëŠ” ê²½ìš°
         return true;
     }
     return false;
