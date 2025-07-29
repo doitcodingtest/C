e161a0f4b9f509911385725daeda1f838bac7001
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2025-07-29 19:42:03 +0900

    Rename 1328_빌딩.cpp to P1328_빌딩.cpp

--- 동적계획법/P1328_빌딩.cpp
+++ 동적계획법/P1328_빌딩.cpp
@@ -9,7 +9,7 @@ static long D[101][101][101];
 int main()
 {
     cin >> N >> L >> R;
-    D[1][1][1] = 1; // ºôµùÀÌ 1°³ÀÌ¸é ³õÀº ¼ö ÀÖ´Â °æ¿ìÀÇ ¼ö´Â 1°³
+    D[1][1][1] = 1; // ë¹Œë”©ì´ 1ê°œì´ë©´ ë†“ì€ ìˆ˜ ìžˆëŠ” ê²½ìš°ì˜ ìˆ˜ëŠ” 1ê°œ
     for (int i = 2; i <= N; i++) {
         for (int j = 1; j <= L; j++) {
             for (int k = 1; k <= R; k++) {
